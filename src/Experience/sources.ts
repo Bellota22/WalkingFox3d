b03e0b09ab1958ac44c1505e7ae9ec296e615ee3
Gabriel <gvillanuevavega@gmail.com>
2025-05-01 22:08:26 -0500

    deleting static, moving into public

--- src/Experience/sources.ts
+++ src/Experience/sources.ts
@@ -4,28 +4,28 @@ export default [
       type: 'cubeTexture',
       path:
       [
-          '/static/textures/environmentMap/px.jpg',
-          '/static/textures/environmentMap/nx.jpg',
-          '/static/textures/environmentMap/py.jpg',
-          '/static/textures/environmentMap/ny.jpg',
-          '/static/textures/environmentMap/pz.jpg',
-          '/static/textures/environmentMap/nz.jpg'
+          '/textures/environmentMap/px.jpg',
+          '/textures/environmentMap/nx.jpg',
+          '/textures/environmentMap/py.jpg',
+          '/textures/environmentMap/ny.jpg',
+          '/textures/environmentMap/pz.jpg',
+          '/textures/environmentMap/nz.jpg'
       ]
   },
   {
     name: 'grassColorTexture',
     type: 'texture',
-    path: '/static/textures/dirt/color.jpg'
+    path: '/textures/dirt/color.jpg'
   },
   {
     name: 'grassNormalTexture',
     type: 'texture',
-    path: '/static/textures/dirt/normal.jpg'
+    path: '/textures/dirt/normal.jpg'
   },
   {
     name: 'foxModel',
     type: 'gltfModel',
-    path: '/static/models/Fox/glTF/Fox.gltf'
+    path: '/models/Fox/glTF/Fox.gltf'
 }
 
 ]
